bf213c97c22247c0091a2992b1bd7d11c1b20ee2
Ruperth <97520685+iRuperth@users.noreply.github.com>
2024-05-20 11:47:20 +0000

    Hecho por roberto, ha sido divertido. :p

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,16 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName} </h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} , ${variables.country} </h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href= https://www.twitter.com/${variables.twitter}><i class="fab fa-twitter"></i></a></li>
+            <li><a href=https://www.github.com/${variables.github}><i class="fab fa-github"></i></a></li>
+            <li><a href=https://linkedin.com/${variables.linkedin}><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=https://instagram.com/${variables.instagram}><i class="fab fa-instagram"></i></a></li>
           </ul>
+          
         </div>
     `;
 }
@@ -54,11 +55,12 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://hips.hearstapps.com/hmg-prod/images/godzilla-vs-kong-poster-8-fotogramas-1615241020.jpg?crop=1.00xw:0.337xh;0,0.168xh&resize=1200:*",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://storage.googleapis.com/pod_public/1300/181500.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
     github: null,
